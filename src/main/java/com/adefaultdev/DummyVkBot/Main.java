537f9387576df72fbb9001150cab4f8f7785ea4f
ADefaultDev <im7ckngmad@gmail.com>
2025-06-22 03:26:14 +0500

    Adjusted Main's logic to use single driver in DeepSeekClient and BrowserListener

--- src/main/java/com/adefaultdev/DummyVkBot/Main.java
+++ src/main/java/com/adefaultdev/DummyVkBot/Main.java
@@ -1,14 +1,23 @@
 package com.adefaultdev.DummyVkBot;
 
 import com.adefaultdev.DummyVkBot.browser.BrowserLauncher;
+import com.adefaultdev.DummyVkBot.browser.BrowserListener;
 import com.adefaultdev.DummyVkBot.dsConnection.DeepSeekClient;
+import org.openqa.selenium.WebDriver;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        BrowserLauncher.launchBrowser();
-        DeepSeekClient.dsClientSetup();
+        WebDriver driver = BrowserLauncher.launchBrowser();
+
+        DeepSeekClient dsClient = new DeepSeekClient(driver);
+        dsClient.setup("https://www.deepseek.com/en");
+
+        BrowserListener listener = new BrowserListener();
+        listener.start("https://vk.com/", driver, dsClient);
 
         //add logic to create path to get ready messages(text and pictures)
+
     }
 }
