0590b4074e04f66a0e26da0f3d82a4247987beac
ADefaultDev <im7ckngmad@gmail.com>
2025-06-23 17:29:03 +0500

    BrowserConfig - commentaries added

--- src/main/java/com/adefaultdev/DummyVkBot/browser/BrowserConfig.java
+++ src/main/java/com/adefaultdev/DummyVkBot/browser/BrowserConfig.java
@@ -3,29 +3,30 @@ package com.adefaultdev.DummyVkBot.browser;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * Using config.properties file to provide necessary settings for other classes
+ */
 public class BrowserConfig {
-
     private static final String PROPERTIES_FILE = "config.properties";
+
     private static final String browserPath;
     private static final String driverVersion;
+
 
     static {
         try (InputStream input = BrowserConfig.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
             Properties prop = new Properties();
             prop.load(input);
+
             browserPath = prop.getProperty("browser.path");
             driverVersion = prop.getProperty("driver.version");
+
         } catch (Exception e) {
             throw new RuntimeException("Failed to load config file", e);
         }
     }
 
-    public static String getBrowserPath() {
-        return browserPath;
-    }
-
-    public static String getDriverVersion() {
-        return driverVersion;
-    }
+    public static String getBrowserPath() { return browserPath; }
+    public static String getDriverVersion() { return driverVersion; }
 
 }
