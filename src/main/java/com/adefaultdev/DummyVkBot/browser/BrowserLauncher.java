2f966f931da1fe35f7908f0553b6fe8613af5fb1
ADefaultDev <im7ckngmad@gmail.com>
2025-06-23 18:08:00 +0500

    BrowserLauncher now have options to bypass bot detection

--- src/main/java/com/adefaultdev/DummyVkBot/browser/BrowserLauncher.java
+++ src/main/java/com/adefaultdev/DummyVkBot/browser/BrowserLauncher.java
@@ -5,11 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+/**
+ * Setups Chrome browser
+ * Using arguments in options to bypass cloudflare and similar protection
+ */
 public class BrowserLauncher {
 
     public static WebDriver launchBrowser() {
+
         ChromeOptions options = new ChromeOptions();
+
         options.setBinary(BrowserConfig.getBrowserPath());
+
+        options.addArguments(
+                "--disable-blink-features=AutomationControlled",
+                "--start-maximized",
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+        );
 
         WebDriverManager.chromedriver()
                 .driverVersion(BrowserConfig.getDriverVersion())
@@ -17,5 +30,4 @@ public class BrowserLauncher {
 
         return new ChromeDriver(options);
     }
-
 }
