aaf4ffce688a8f60542d24d695dbc35c170f82d4
ADefaultDev <im7ckngmad@gmail.com>
2025-06-25 02:25:36 +0500

    Implementing logback-classic now. It's not messing with cloudflare bypassing

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 
     implementation("org.seleniumhq.selenium:selenium-java:4.15.0")
     implementation("io.github.bonigarcia:webdrivermanager:6.1.0")
+    implementation ("ch.qos.logback:logback-classic:1.5.13")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
