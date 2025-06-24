05c12f28f74d3e6470b699d672dfb10c22e171f8
ADefaultDev <im7ckngmad@gmail.com>
2025-06-25 02:32:21 +0500

    BrowserWatcher refactoring

--- src/main/java/com/adefaultdev/DummyVkBot/browser/BrowserWatcher.java
+++ src/main/java/com/adefaultdev/DummyVkBot/browser/BrowserWatcher.java
@@ -2,21 +2,25 @@ package com.adefaultdev.DummyVkBot.browser;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Setups watcher for browser, making program stop if browser stopped manually
+ */
 @SuppressWarnings("BusyWait")
 public class BrowserWatcher implements Runnable {
     private final WebDriver driver;
 
     public BrowserWatcher(WebDriver driver) {
+
         this.driver = driver;
+
     }
 
     @Override
     public void run() {
+
         try {
             while (true) {
-
                 if (driver.getWindowHandles().isEmpty()) {
-                    System.out.println("Browser quit...");
                     System.exit(0);
                 }
                 Thread.sleep(1000); // Had to check in thread due to lack of API
@@ -25,4 +29,5 @@ public class BrowserWatcher implements Runnable {
             System.exit(1);
         }
     }
+
 }
