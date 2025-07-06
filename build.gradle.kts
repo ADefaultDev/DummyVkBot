189eb0d3ee6d3097ada21f4140edb2b20d0a96ae
ADefaultDev <im7ckngmad@gmail.com>
2025-07-07 03:12:01 +0500

    Gradle build adjusted to implement mockito and junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,9 @@ dependencies {
     implementation("io.github.bonigarcia:webdrivermanager:6.1.0")
     implementation ("ch.qos.logback:logback-classic:1.5.13")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.mockito:mockito-core:5.3.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
 
 }
 
